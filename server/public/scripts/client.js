e90be61e0361994a77957a2da25b8031bbb56c98
Luke Becker <beckerluke1@gmail.com>
2019-08-09 16:52:10 -0500

    Added working post request and pushed req.body into inputNumbers array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,15 +2,38 @@ $(document).ready(init);
 
 function init () {
     console.log('What up');
+
+    $('#js-btn-submit').on('click', postInputNumbers);
     
-    getDemo();
+    postInputNumbers();
 }
 
-function getInputNumbers() {
+function postInputNumbers() {
+    $('#js-btn-submit').on('click', postInputNumbers);
+    const dataForServer = {
+        firstNumber: $('#first-number-input').val(),
+        secondNumber: $('#second-number-input').val(), 
+    }; 
+
+    console.log(dataForServer);
+    
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: '/input-numbers',
+        data: dataForServer,
     }).then((response) => {
         console.log(response);
+        // getCalculation();
     });
 }
+
+// function getInputNumbers() {
+//     $.ajax({
+//         type: 'GET',
+//         url: '/input-numbers',
+//     }).then(function(response) {
+//         console.log();
+        
+//        render(response);
+//     });
+// }
--- server/server.js
+++ server/server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 5000;
+const inputNumbers = [];
+// {
+//     firstNumber: parseInt(req.body.firstNumber),
+//     secondNumber: parseInt(req.body.secondNumber),
+// }];
+console.log(inputNumbers);
+
 
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/input-numbers', (req,res) => {
+app.post('/input-numbers', (req,res) => {
+    const newInputNumbers = req.body;
+    inputNumbers.push(newInputNumbers); 
+    
     console.log('GET ROUTE');
+    console.log(inputNumbers);
     res.send('GOT IT');
 });
 
