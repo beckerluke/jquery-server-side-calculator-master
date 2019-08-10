2d884ac4bc5abe878838e2aef17977de5d70f5ea
Luke Becker <beckerluke1@gmail.com>
2019-08-09 19:55:38 -0500

    changed dataForServer variable to userInputToCalculate and made it global. Added assignSymbol function to identify what symbol was clicked to send to the server. Added click event for assignSymbol in init function.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,39 +1,61 @@
+const userInputToCalculate = {
+    firstNumber: null,
+    secondNumber: null, 
+    symbol: null,
+}; 
+
 $(document).ready(init); 
 
 function init () {
     console.log('What up');
 
-    $('#js-btn-submit').on('click', postInputNumbers);
+    $('.js-btn-math-symbol').on('click', assignSymbol); // event listener when you click on one of the math symbol buttons
+    $('#js-btn-submit').on('click', postInputNumbers); // event listener when you click submit button 
     
-    postInputNumbers();
 }
 
-function postInputNumbers() {
-    $('#js-btn-submit').on('click', postInputNumbers);
-    const dataForServer = {
-        firstNumber: $('#first-number-input').val(),
-        secondNumber: $('#second-number-input').val(), 
-    }; 
-
-    console.log(dataForServer);
+function assignSymbol() {
+    const mathSymbolClicked = $(this).data('symbol')
+    console.log(mathSymbolClicked);
     
-    $.ajax({
-        type: 'POST',
-        url: '/input-numbers',
-        data: dataForServer,
-    }).then((response) => {
-        console.log(response);
-        // getCalculation();
-    });
-}
+//     userInputToCalculate.symbol = $(this).data('symbol'); // symbol user clicks on
+// }
 
 // function getInputNumbers() {
 //     $.ajax({
 //         type: 'GET',
-//         url: '/input-numbers',
+//         url: '/calculate',
 //     }).then(function(response) {
 //         console.log();
         
 //        render(response);
 //     });
 // }
+
+// function postInputNumbers() {
+//     const dataForServer = {
+//         firstNumber: $('#first-input-number').val(),
+//         secondNumber: $('#second-input-number').val(), 
+//     }; 
+
+//     console.log(dataForServer);
+    
+//     $.ajax({
+//         type: 'POST',
+//         url: '/calculate',
+//         data: userInputToCalculate,
+//     }).then((response) => {
+//         console.log(response);
+//         // getCalculation();
+//     });
+// }
+
+// function submitNumbers() {
+//     if ( $('#first-input-number').val() && $('#second-input-number').val() != '') {
+//         userInputToCalculate.firstNumber = $('#first-input-number').val(); 
+//         userInputToCalculate.secondNumber = $('#second-input-number').val();
+//         postInputNumbers();
+//     } else {
+//         alert('Enter Both Numbers');
+//     }
+} // put values from input fields into userInputToCalculate object and post. Alert if user submits without filling out fields  
--- server/server.js
+++ server/server.js
@@ -2,26 +2,40 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 5000;
-const inputNumbers = [];
+const inputNumbersArray = [];
+const calculatedOutput; // variable to hold number after math has been calculated
 // {
 //     firstNumber: parseInt(req.body.firstNumber),
 //     secondNumber: parseInt(req.body.secondNumber),
 // }];
-console.log(inputNumbers);
+console.log(inputNumbersArray);
 
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.post('/input-numbers', (req,res) => {
-    const newInputNumbers = req.body;
-    inputNumbers.push(newInputNumbers); 
+app.get('/calculate', (req,res) => {
+    res.send(calculatedOutput); // sending back inputNumbersArray
+});
+
+app.post('/calculate', (req,res) => {
+    const newInputNumbersToCalculate = {
+        firstNumber: parseInt(req.body.firstNumber), // first number entered
+        secondNumber: parseInt(req.body.secondNumber), // second number entered
+        symbol: req.body.symbol, // math symbol selected before hitting submit
+    }; // object to convert and store strings into numbers from post request before being pushed into inputNumbersArray
+
+    inputNumbersArray.push(newInputNumbersToCalculate); 
     
     console.log('GET ROUTE');
-    console.log(inputNumbers);
+    console.log(inputNumbersArray);
     res.send('GOT IT');
 });
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
 });
+
+// function calculateMath(inputNumbersArray) {
+//     if ( )
+// }
